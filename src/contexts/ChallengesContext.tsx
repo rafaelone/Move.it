5471baa18cfdd71284beebca8ac904e242bdbdcc
Rafael Sergio <rafael.sergio@lumis.com.br>
2021-02-27 15:24:39 -0300

    storage via cookies

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useCallback, useEffect, useMemo, useState } from "react"
+import Cookies from "js-cookie"
 import challenges from "../../challenges.json"
 
 interface Challenge {
@@ -20,17 +21,28 @@ interface ChallengesContextData {
 }
 
 interface ChallengesProviderProps {
-  children: ReactNode
+  children: ReactNode;
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData)
 
-export function ChallengesProvider({ children }: ChallengesProviderProps){
-  const [ level, setLevel ] = useState(1);
-  const [ currentExperience, setCurrentExperience ] = useState(0);
-  const [ challengesCompleted, setChallengesCompleted ] = useState(0);
+export function ChallengesProvider({ children,
+  ...rest
+}: ChallengesProviderProps){
+  const [ level, setLevel ] = useState(rest.level ?? 1);
+  const [ currentExperience, setCurrentExperience ] = useState(rest.currentExperience ?? 0);
+  const [ challengesCompleted, setChallengesCompleted ] = useState(rest.challengesCompleted ??0);
 
   const [ activeChallenge, setActiveChallenge ] = useState(null)
+
+  useEffect(() => {
+      Cookies.set('level', String(level));
+      Cookies.set('currentExperience', String(currentExperience));
+      Cookies.set('challengesCompleted', String(challengesCompleted));
+  }, [level, currentExperience, challengesCompleted])
 
   useEffect(() => {
     Notification.requestPermission()
